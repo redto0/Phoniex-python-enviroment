99e9b9edfb1d94eed82c227f31e8dd3212fb9c84
redto0 <aboccacc@umich.edu>
2025-01-18 18:27:58 -0500

    update

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ inter = 0
 with open( "/home/alex/CLionProjects/untitled/Data/Datav4.txt" , 'r' ) as file:
     #read by lines
     for line in file:
-        print(line)
+        # print(line)
         # split by whitespace
-        coeffs = line.split()
+        splitter = line.split()
+        for i in range(4):
+            coeffs.append( float(splitter[i]) )
         #print()
-print(coeffs)
+# print(coeffs)
 xpoints = np.array(xpoints)
 ypoints = np.array(ypoints)
 
@@ -41,18 +43,28 @@ plt.ylabel('Y-axis')  # Add y-axis label
 plt.title('Data Plot')  # Add a title
 #plt.grid(True)  # Add a grid for better readability
 
-# Create a smooth x array for the polynomial
-# x_smooth = np.linspace(min(xpoints), max(xpoints), 200)
-
 
 # Create a smooth x array for the polynomial
 num_of_x = 200
-x_smooth = np.linspace(min(xpoints), max(xpoints), num_of_x)
+# x_smooth = np.linspace(min(xpoints), max(xpoints), num=num_of_x )
+x_smooth = []
+for i in range(num_of_x):
+    x_smooth.append(i/num_of_x * 550)
+print(max(x_smooth))
+print(min(x_smooth))
 # Calculate the y values for the polynomial
+#y_poly = []
+# coeffs = [2.81620753e-08, -3.66257507e-05S,  3.98234042e-01,  1.53898013e+02]
+print(type(coeffs))
+print("coeffs: ")
+print( coeffs)
 y_poly = []
-coeffs = [2.81620753e-08, -3.66257507e-05,  3.98234042e-01,  1.53898013e+02]
+for x in x_smooth:
+    y_poly.append(coeffs[0] * x ** 3 + coeffs[1] * x ** 2 + coeffs[2] * x ** 1 + coeffs[3] )
 
-y_poly = np.polyval(coeffs, x_smooth)
+print(max(coeffs))
+print(min(coeffs))
+#y_poly = np.polyval(coeffs, x_smooth)
 
 # Plot original data
 plt.scatter(xpoints, ypoints, color='blue', label='Original Data')
